d7675df547fc485d6f7d567a67785e768851080f
Marc <Marc.com>
2016-12-02 14:43:48 +0100

    2e commit

--- src/main/java/org/formation/jsf/util/TestServlet.java
+++ src/main/java/org/formation/jsf/util/TestServlet.java
@@ -12,6 +12,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 import javax.sql.DataSource;
 
 @WebServlet("/TestServlet")
